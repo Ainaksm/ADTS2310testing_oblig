2340f725f553b1e221b9a467b0c6d8a3e14e3567
Aina <s338849@oslomet.no>
2023-01-20 13:48:47 +0100

    Laget tester for hentAlle i EnhettestKundeController

--- src/test/java/oslomet/testing/EnhettestKundeController.java
+++ src/test/java/oslomet/testing/EnhettestKundeController.java
@@ -37,5 +37,37 @@ public class EnhettestKundeController {
     @Test
     public void hentAlle_loggetInn() {
         // arrange
+        List<Kunde> kunder = new ArrayList<>();
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+        Kunde annenKunde = new Kunde("09090098765",
+                "Geir", "Sol", "Spikkestadveien 22", "3430",
+                "Spikkestad", "66663333", "JaJaJa");
+        kunder.add(enKunde);
+        kunder.add(annenKunde);
+
+        when(sjekk.loggetInn()).thenReturn(enKunde.getPersonnummer(),annenKunde.getPersonnummer());
+        when(repository.hentAlleKunder()).thenReturn(kunder);
+
+        // act
+        List<Kunde> resultat = kundeController.hentAlle();
+
+        // assert
+        assertEquals(kunder, resultat);
     }
+
+    @Test
+    public void hentAlle_ikkeInnlogget() {
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Kunde> resultat = kundeController.hentAlle();
+
+        // assert
+        assertNull(resultat);
+    }
+
+
 }
